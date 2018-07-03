b5cde14ef27ff3dbf21ded1a97f4ac074f65cc3c
maixiem <vuong046@umn.edu>
2018-07-03 10:11:56 -0500

    Update game.js

--- mfgj2018/game.js
+++ mfgj2018/game.js
@@ -1,1 +1,14 @@
+/*Resources for learning phaser 2:
+http://phaser.io/tutorials/making-your-first-phaser-2-game
+http://phaser.io/examples
+*/
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
